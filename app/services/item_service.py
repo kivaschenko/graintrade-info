f1efb1c0d8e2c86d526eee17445b32b7550eab34
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-08-09 12:06:12 +0300

    add doc string

--- app/services/item_service.py
+++ app/services/item_service.py
@@ -9,6 +9,8 @@ from app.services.notification_service import NotificationService
 
 
 class ItemApplicationService:
+    """The service layer is responsible for the application's business logic.
+    """
     def __init__(
         self,
         item_repository: AbstractItemRepository,
@@ -24,6 +26,7 @@ class ItemApplicationService:
         recipient: Recipient = None,
         background_tasks: BackgroundTasks = None,
     ) -> ItemInResponse:
+        """Create an item and send notifications to the recipient(s) if provided."""
         if username is None:
             raise ValueError("Username is required")
 
@@ -31,15 +34,14 @@ class ItemApplicationService:
         created_item = await self.item_repository.create(item, username)
 
         # Add some logic to the item service
-        if self.item_service:
-            self.item_service.bind_views_counter(created_item)
-            # add more logic here
+        # add more logic here
 
         # For example, check the list of subscribers of username and send notifications
         # Send notification if recipient(s) is provided
-        if recipient and self.notification_service and background_tasks:
-            background_tasks.add_task(
-                self.notification_service.send_notification,
+        if recipient and self.notification_services and background_tasks:
+            for notification_service in self.notification_services:
+                background_tasks.add_task(
+                notification_service.send_notification,
                 recipient,
                 f"Item {item.title} created",
             )
