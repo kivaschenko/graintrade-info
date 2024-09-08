8e90ef013179baad801ac3dfca9a29990a365445
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-09-08 17:31:22 +0300

    save changes

--- subscription-service/app/schemas.py
+++ subscription-service/app/schemas.py
@@ -2,26 +2,6 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 
 
-class SubscriptionInDB(BaseModel):
-    user_id: int
-    tarif_id: int
-    start_date: datetime
-    end_date: datetime
-    status: str
-
-
-class SubscriptionInResponse(SubscriptionInDB):
-    id: int
-    created_at: datetime = Field(alias="created_at")
-
-    @property
-    def formatted_created_at(self) -> str:
-        return self.created_at.isoformat()
-
-    class ConfigDict:
-        from_attributes = True
-
-
 class TarifInDB(BaseModel):
     name: str
     description: str
@@ -34,6 +14,27 @@ class TarifInDB(BaseModel):
 class TarifInResponse(TarifInDB):
     id: int
     created_at: datetime = Field(alias="created_at")
+
+    @property
+    def formatted_created_at(self) -> str:
+        return self.created_at.isoformat()
+
+    class ConfigDict:
+        from_attributes = True
+
+
+class SubscriptionInDB(BaseModel):
+    user_id: int
+    tarif_id: int
+    start_date: datetime
+    end_date: datetime
+    status: str
+
+
+class SubscriptionInResponse(SubscriptionInDB):
+    id: int
+    created_at: datetime = Field(alias="created_at")
+    tarif: TarifInResponse
 
     @property
     def formatted_created_at(self) -> str:
