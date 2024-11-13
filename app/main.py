604e4bd7f710b8e9e49c85b212d32dee064bb9f6
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-11-13 17:23:38 +0200

    tested all endpoints

--- app/main.py
+++ app/main.py
@@ -3,7 +3,15 @@ from datetime import timedelta, datetime, timezone
 from typing import Annotated, List
 import logging
 
-from fastapi import FastAPI, Depends, HTTPException, Security, status, BackgroundTasks
+from fastapi import (
+    FastAPI,
+    Depends,
+    HTTPException,
+    Security,
+    status,
+    BackgroundTasks,
+    Query,
+)
 from fastapi.security import (
     OAuth2PasswordBearer,
     OAuth2PasswordRequestForm,
@@ -355,7 +363,7 @@ async def create_item(
     if "create:item" not in scopes:
         logging.error("Not enough permissions")
         raise HTTPException(status_code=403, detail="Not enough permissions")
-    new_item = await repo.create(item)
+    new_item = await repo.create(item, user_id)
     if new_item is None:
         logging.error("Item not created")
         raise HTTPException(status_code=400, detail="Item not created")
@@ -389,62 +397,50 @@ async def read_item(
     return db_item
 
 
-@app.put(
-    "/items/{item_id}",
-    response_model=ItemInResponse,
-    status_code=status.HTTP_202_ACCEPTED,
-    tags=["Items"],
-)
+@app.put("/items/{item_id}", response_model=ItemInResponse, tags=["Items"])
 async def update_item(
     item_id: int,
     item: ItemInDB,
+    background_tasks: BackgroundTasks,
     repo: AsyncpgItemRepository = Depends(get_item_repository),
     token: Annotated[str, Depends(oauth2_scheme)] = None,
 ):
     if token is None:
         logging.error("No token provided")
         raise HTTPException(status_code=401, detail="Invalid token")
-    user_id = await get_current_user_id(token)
-    db_item = await repo.update(item_id, user_id, item)
-    if db_item is None:
-        logging.error(f"Item with id {item_id} not found")
-        raise HTTPException(status_code=404, detail="Item not found")
-    return db_item
+    user_id, scopes = await get_current_user_id(token)
+    print(f"User ID: {user_id}, Scopes: {scopes}")
+    if "update:item" not in scopes:
+        logging.error("Not enough permissions")
+        raise HTTPException(status_code=403, detail="Not enough permissions")
+    updated_item = await repo.update(item_id, user_id, item)
+    if updated_item is None:
+        logging.error("Item not updated")
+        raise HTTPException(status_code=400, detail="Item not updated")
+    return updated_item
 
 
-@app.delete(
-    "/items/{item_id}",
-    response_model=dict,
-    status_code=status.HTTP_200_OK,
-    tags=["Items"],
-)
-async def delete_item_bound_to_user(
+@app.delete("/items/{item_id}", status_code=200, tags=["Items"])
+async def delete_item(
     item_id: int,
+    repo: AsyncpgItemRepository = Depends(get_item_repository),
     token: Annotated[str, Depends(oauth2_scheme)] = None,
-    item_repo: AsyncpgItemRepository = Depends(get_item_repository),
 ):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+    if token is None:
+        logging.error("No token provided")
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id, scopes = await get_current_user_id(token)
+    print(f"User ID: {user_id}, Scopes: {scopes}")
+    # check scope and permissions here
+    if "delete:item" not in scopes:
+        logging.error("Not enough permissions")
+        raise HTTPException(status_code=403, detail="Not enough permissions")
     try:
-        payload = jwt.decode(token, settings.jwt_secret, algorithms=["HS256"])
-        user_id = payload.get("user_id")
-        if not user_id:
-            logging.error("No user_id found in token")
-            raise credentials_exception
-        await item_repo.delete(user_id, item_id)
-        return {"status": "success", "message": "Item deleted successfully"}
-    except jwt.ExpiredSignatureError:
-        logging.error("Token has expired")
-        return {"status": "error", "message": "Token has expired"}
-    except jwt.InvalidTokenError:
-        logging.error("Invalid token")
-        return {"status": "error", "message": "Invalid token"}
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        return {"status": "error", "message": "An error occurred: " + str(e)}
+        await repo.delete(item_id, user_id)
+    except ValueError as e:
+        logging.error(e)
+        raise HTTPException(status_code=400, detail=str(e))
+    return {"status": "success", "message": "Item deleted successfully"}
 
 
 # ---------------------
@@ -461,47 +457,62 @@ async def read_items_by_user(
     return await repo.get_items_by_user_id(user_id)
 
 
-@app.get(
-    "/find-items-in-distance",
-    response_model=List[ItemInResponse],
-    tags=["filter items"],
-)
-async def find_items_in_radius(
-    latitude: float,
-    longitude: float,
-    distance: int,  # in meters
+@app.get("/find-items-in-distance", response_model=List[ItemInResponse], tags=["Items"])
+async def find_items_in_distance(
+    latitude: float = Query(..., description="Latitude of the point"),
+    longitude: float = Query(..., description="Longitude of the point"),
+    distance: int = Query(..., description="Distance in meters"),
     repo: AsyncpgItemRepository = Depends(get_item_repository),
+    token: Annotated[str, Depends(oauth2_scheme)] = None,
 ):
-    """Find all items within a given distance from a given point. The distance is in meters."""
-    return await repo.find_in_distance(
-        longitude=longitude, latitude=latitude, distance=distance
-    )
+    if token is None:
+        logging.error("No token provided")
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id, scopes = await get_current_user_id(token)
+    print(f"User ID: {user_id}, Scopes: {scopes}")
+    # check scope and permissions here
+    if "read:item" not in scopes:
+        logging.error("Not enough permissions")
+        raise HTTPException(status_code=403, detail="Not enough permissions")
+    items = await repo.find_in_distance(longitude, latitude, distance)
+    return items
 
 
-@app.get("/filter-items", response_model=List[ItemInResponse], tags=["filter items"])
+@app.get("/filter-items", response_model=List[ItemInResponse], tags=["Items"])
 async def filter_items(
-    min_price: float = None,
-    max_price: float = None,
-    currency: str = None,
-    min_amount: int = None,
-    max_amount: int = None,
-    measure: str = None,
-    terms_delivery: str = None,
-    country: str = None,
-    region: str = None,
+    min_price: float = Query(None, description="Minimum price"),
+    max_price: float = Query(None, description="Maximum price"),
+    currency: str = Query(None, description="Currency"),
+    min_amount: int = Query(None, description="Minimum amount"),
+    max_amount: int = Query(None, description="Maximum amount"),
+    measure: str = Query(None, description="Measure"),
+    terms_delivery: str = Query(None, description="Terms of delivery"),
+    country: str = Query(None, description="Country"),
+    region: str = Query(None, description="Region"),
     repo: AsyncpgItemRepository = Depends(get_item_repository),
+    token: Annotated[str, Depends(oauth2_scheme)] = None,
 ):
-    return await repo.get_filtered_items(
-        min_price=min_price,
-        max_price=max_price,
-        currency=currency,
-        min_amount=min_amount,
-        max_amount=max_amount,
-        measure=measure,
-        terms_delivery=terms_delivery,
-        country=country,
-        region=region,
+    if token is None:
+        logging.error("No token provided")
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id, scopes = await get_current_user_id(token)
+    print(f"User ID: {user_id}, Scopes: {scopes}")
+    # check scope and permissions here
+    if "read:item" not in scopes:
+        logging.error("Not enough permissions")
+        raise HTTPException(status_code=403, detail="Not enough permissions")
+    items = await repo.get_filtered_items(
+        min_price,
+        max_price,
+        currency,
+        min_amount,
+        max_amount,
+        measure,
+        terms_delivery,
+        country,
+        region,
     )
+    return items
 
 
 # @app.post("/items/{item_id}/like", response_model=dict, tags=["Items"])
@@ -516,16 +527,3 @@ async def filter_items(
 #     user_id = await get_current_user_id(token)
 #     await repo.like_item(user_id, item_id)
 #     return {"status": "success", "message": "Item liked successfully"}
-
-
-@app.post("items-many/", response_model=List[ItemInResponse], tags=["Items"])
-async def create_many_items(
-    items: List[ItemInDB],
-    repo: AsyncpgItemRepository = Depends(get_item_repository),
-    token: Annotated[str, Depends(oauth2_scheme)] = None,
-):
-    if token is None:
-        logging.error("No token provided")
-        raise HTTPException(status_code=401, detail="Invalid token")
-    user_id = await get_current_user_id(token)
-    return await repo.create_many(items, user_id)
