ea82ca4974f325e80ef7e5f4c3ed65939580c370
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2025-04-07 23:08:23 +0300

    save between debug

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ BASE_DIR: Path = Path(__file__).resolve().parent.parent
 ENV_FILE: Path = BASE_DIR / ".env"
 
 
+# TODO: remove this and excchange with import dotenv in root __init__.py
 class Settings(BaseSettings):
     postgres_user: str
     postgres_password: str
@@ -18,22 +19,22 @@ class Settings(BaseSettings):
     rabbitmq_port: int
     rabbitmq_default_user: str
     rabbitmq_default_pass: str
-    rabbitmq_vhost: str = "/"  # Default RabbitMQ virtual host
+    rabbitmq_vhost: str
     app_name: str = "GRAINTRADE.INFO API | Graintrade Resource Group"
     app_version: str = "0.1.0"
     DATABASE_URL: Optional[str] = None
     RABBITMQ_URL: Optional[str] = None
-    RABBITMQ_QUEUE_NAME: Optional[str] = None
 
     def model_post_init(self, __context: Any) -> None:
+        """Override this method to perform additional initialization after `__init__`
+        and `model_construct`. This is useful if you want to do some validation that
+        requires the entire model to be initialized.
+        """
         if not self.DATABASE_URL:
-            self.DATABASE_URL = f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
+            self.DATABASE_URL = f"postgresql://{self.pguser}:{self.pgpassword}@{self.pghost}:{self.pgport}/{self.pgdatabase}"
 
         if not self.RABBITMQ_URL:
-            self.RABBITMQ_URL = f"amqp://{self.rabbitmq_default_user}:{self.rabbitmq_default_pass}@{self.rabbitmq_host}:{self.rabbitmq_port}/"
-
-        if not self.RABBITMQ_QUEUE_NAME:
-            self.RABBITMQ_QUEUE_NAME = "graintrade_queue"
+            self.RABBITMQ_URL = f"amqp://{self.rabbitmq_user}:{self.rabbitmq_password}@{self.rabbitmq_host}:{self.rabbitmq_port}/{self.rabbitmq_vhost}"
 
     class Config:
         env_file = ENV_FILE
