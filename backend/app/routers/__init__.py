2a7b54644412a08b659d944e3d185077296626d1
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2025-06-10 13:57:37 +0300

    Merge branch 'rabbitmq-instead-kafka'

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 JWT_SECRET = os.getenv(
     "JWT_SECRET",
@@ -7,3 +8,8 @@ JWT_SECRET = os.getenv(
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("JWT_EXPIRES_IN", 60)
 MAP_VIEW_LIMIT = 100
+
+logging.info("Initializing user router...")
+logging.info(f"JWT_SECRET: {JWT_SECRET}")
+logging.info(f"ALGORITHM: {ALGORITHM}")
+logging.info(f"ACCESS_TOKEN_EXPIRE_MINUTES: {ACCESS_TOKEN_EXPIRE_MINUTES}")
