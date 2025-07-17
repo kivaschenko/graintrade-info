62c73017339fe013a13697fcb890b849f51b0c96
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2025-07-17 18:05:05 +0300

    working draft to send new message to rabbitmq work queue

--- chat-room/app/services.py
+++ chat-room/app/services.py
@@ -1,18 +1,25 @@
 import logging
 from .rabbit_mq import rabbitmq, QueueName
+from .models import Message
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def send_message_to_queue(
-    message: dict, queue: QueueName = QueueName.MESSAGE_EVENTS
+    message: Message, queue: QueueName = QueueName.MESSAGE_EVENTS
 ):
     try:
         await rabbitmq.connect()
-        await rabbitmq.publish(message, queue=queue)
+        message_body = {
+            "type": "new_message",
+            "to_username": message.receiver_id,
+            "message_text": message.content,
+            "item_id": message.item_id,
+        }
+        await rabbitmq.publish(message=message_body, queue=queue)
     except Exception as e:
         logging.error(f"Failed to send message to RabbitMQ: {e}")
     finally:
         await rabbitmq.close()
-        logging.info(f"Message ID {message['id']} sent to queue.")
+        logging.info(f"Message ID {message.id} sent to queue.")
         # Ensure the connection is closed
